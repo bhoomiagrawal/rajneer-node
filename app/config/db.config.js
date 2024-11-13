7a11c3188cfe775bd5740ad7fc98c5db853fcce7
Bhoomi <agrawalbhoomika16@gmail.com>
2024-11-13 16:50:41 +0530

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  HOST: "10.68.231.131",
+  USER: "root",
+  PASSWORD: "Admin@123#",
+  DB: "rajneer2",
+  dialect: "mysql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
