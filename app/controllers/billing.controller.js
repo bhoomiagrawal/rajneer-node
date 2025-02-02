359ca289e6016c8474d85fbdda33a2c0640c4caf
bhoomiagrawal <agrawalbhoomika16@gmail.com>
2025-02-02 15:50:24 +0530

    fix: flat category generate bill harcode

--- app/controllers/billing.controller.js
+++ app/controllers/billing.controller.js
@@ -27,7 +27,6 @@ const getStpCharge = async (waterCharge) => {
   return parseFloat(((waterCharge * stpPercent) / 100).toFixed(2));
 };
 
-
 const idc = [
   { min: 15001, max: 40000, chargePercent: 25 },
   { min: 40001, max: Infinity, chargePercent: 35 },
@@ -63,7 +62,7 @@ const getIDC = (consumption, bill) => {
 //     let min_consumption = idc.slab.min_consumption || 0;
 //     let max_consumption = idc.max_consumption || Infinity;
 //     return (
-    
+
 //       consumption >= min_consumption &&
 //       consumption <= max_consumption
 //     );
@@ -83,6 +82,7 @@ const calculateWaterCharges = async ({
   consumption,
   connection_type_id,
   isBulk,
+  billPayload,
 }) => {
   try {
     let tariffs = [];
@@ -97,7 +97,7 @@ const calculateWaterCharges = async ({
     } else {
       // Non-bulk tariff calculation
       tariffs = await db.tariffConfiguration.findAll({
-        where: { category_id, charge_type_id: 1, connection_size_id:1 },
+        where: { category_id, charge_type_id: 1, connection_size_id: 1 },
         include: [
           {
             model: db.slabs,
@@ -108,11 +108,12 @@ const calculateWaterCharges = async ({
         order: [[{ model: db.slabs, as: "slab" }, "min_consumption", "ASC"]],
       });
     }
-    console.log("tariffs value is :-", tariffs);
+    console.log("tariffs value is :-", tariffs, billPayload);
     if (!tariffs || tariffs.length === 0) {
-      throw new Error(
-        "No water tariff configurations found for the given category/connection size or bulk status."
-      );
+      console.log('error:::::', "No water tariff configurations found for the given category/connection size or bulk status.")
+      // throw new Error(
+      //   "No water tariff configurations found for the given category/connection size or bulk status."
+      // );
     }
     let totalWaterCharge = 0;
     let remainingConsumption = consumption;
@@ -122,76 +123,97 @@ const calculateWaterCharges = async ({
 
     for (const tariff of tariffs) {
       const { ratePerThousand } = tariff;
-        const { slab } = tariff;
-        console.log("slab::::::::", slab)
-        let  slabMin = slab?.min_consumption || 0;
-        let  slabMax = slab?.max_consumption == null || slab?.max_consumption == "" ? Infinity : slab?.max_consumption;
-        if(slab)slab.ratePerThousand = ratePerThousand;
-        
-        consumptionSlabs.push(slab);
+      const { slab } = tariff;
+      console.log("slab::::::::", slab);
+      let slabMin = slab?.min_consumption || 0;
+      let slabMax =
+        slab?.max_consumption == null || slab?.max_consumption == ""
+          ? Infinity
+          : slab?.max_consumption;
+      if (slab) slab.ratePerThousand = ratePerThousand;
 
-     
-        console.log("consumption::::::::", consumption)
-        console.log("slabMax::::::::", slabMax)
-        if(!isBulk) {
+      consumptionSlabs.push(slab);
 
-          if (consumption <= slabMax) {
-          
-              console.log("entring in consumption <= slabMax part:::::: consumption is,", consumption, "and slabMax is::::", slabMax, "::::::")
-              console.log("Slabmax values is:::", slabMax, "   and previousmax vlues is:::: ", previousMax, "   so the difference is::: ", consumption - previousMax, "   : for rate per thousand is::", ratePerThousand, "  ::")
-              bCharge += ((consumption - previousMax) / 1000) * ratePerThousand;
-  
-              console.log("bCharge::::::::", bCharge)
-            break;
-          } else {
-  
-            console.log("entring in else part::::::")
-            console.log("Slabmax values is:::", slabMax, "   and previousmax vlues is:::: ", previousMax, "   so the difference is::: ", slabMax - previousMax, "   : for rate per thousand is::", ratePerThousand, "  ::")
-            bCharge += ((slabMax - previousMax) / 1000) * ratePerThousand;
-  
-            previousMax = slabMax;
-  
-            console.log("bCharge:::::::: in else part", bCharge)
-          console.log("previousMax::::::::", previousMax)
-  
-          }
+      console.log("consumption::::::::", consumption);
+      console.log("slabMax::::::::", slabMax);
+      if (!isBulk) {
+        if (consumption <= slabMax) {
+          console.log(
+            "entring in consumption <= slabMax part:::::: consumption is,",
+            consumption,
+            "and slabMax is::::",
+            slabMax,
+            "::::::"
+          );
+          console.log(
+            "Slabmax values is:::",
+            slabMax,
+            "   and previousmax vlues is:::: ",
+            previousMax,
+            "   so the difference is::: ",
+            consumption - previousMax,
+            "   : for rate per thousand is::",
+            ratePerThousand,
+            "  ::"
+          );
+          bCharge += ((consumption - previousMax) / 1000) * ratePerThousand;
 
+          console.log("bCharge::::::::", bCharge);
+          break;
         } else {
+          console.log("entring in else part::::::");
+          console.log(
+            "Slabmax values is:::",
+            slabMax,
+            "   and previousmax vlues is:::: ",
+            previousMax,
+            "   so the difference is::: ",
+            slabMax - previousMax,
+            "   : for rate per thousand is::",
+            ratePerThousand,
+            "  ::"
+          );
+          bCharge += ((slabMax - previousMax) / 1000) * ratePerThousand;
 
-            bCharge = (consumption / 1000) * ratePerThousand;
-            console.log("bCharge", bCharge)
+          previousMax = slabMax;
+
+          console.log("bCharge:::::::: in else part", bCharge);
+          console.log("previousMax::::::::", previousMax);
         }
-       
+      } else {
+        bCharge = (consumption / 1000) * ratePerThousand;
+        console.log("bCharge", bCharge);
+      }
+    }
+    if(category_id == 4) {
+      bCharge = (consumption / 1000) * 27.5;
     }
     totalWaterCharge = bCharge;
-  //       console.log("slab:::::::", slab);
-  //       console.log("slabMin:::::::", slabMin);
-  //       console.log("slabMax:::::::", slabMax);
-        
-  //     }
+    //       console.log("slab:::::::", slab);
+    //       console.log("slabMin:::::::", slabMin);
+    //       console.log("slabMax:::::::", slabMax);
 
-  //     let applicableConsumption = Math.min(
-  //       remainingConsumption,
-  //       slabMax - slabMin
-  //     );
+    //     }
 
-  //     if (applicableConsumption > 0) {
-  //       totalWaterCharge += (applicableConsumption / 1000) * ratePerThousand;
-  //       remainingConsumption -= applicableConsumption;
-  //     }
-  //     console.log('applicableConsumption', applicableConsumption);
-  // console.log('remainingConsumption', remainingConsumption);
-  //     if (remainingConsumption <= 0) break;
+    //     let applicableConsumption = Math.min(
+    //       remainingConsumption,
+    //       slabMax - slabMin
+    //     );
+
+    //     if (applicableConsumption > 0) {
+    //       totalWaterCharge += (applicableConsumption / 1000) * ratePerThousand;
+    //       remainingConsumption -= applicableConsumption;
+    //     }
+    //     console.log('applicableConsumption', applicableConsumption);
+    // console.log('remainingConsumption', remainingConsumption);
+    //     if (remainingConsumption <= 0) break;
 
     if (connection_type_id === 2) {
       totalWaterCharge *= 1.5; // Apply 1.5x multiplier for tenant connections
     }
 
     return { waterCharges: totalWaterCharge, consumptionSlabs };
-  }
-
-   
-   catch (error) {
+  } catch (error) {
     console.error("Error in calculateWaterCharges:", error.message);
     throw error;
   }
@@ -199,10 +221,9 @@ const calculateWaterCharges = async ({
 
 // Main Bill Generation Function
 exports.generateBill = async (req, res) => {
-
   const { billingData } = req.body; // Accept an array of billing data objects.
- 
-console.log('billingData', billingData)
+
+  console.log("billingData", billingData);
   // const {
   //   category_id: originalCategoryId,
   //   connection_size_id,
@@ -215,191 +236,204 @@ console.log('billingData', billingData)
   // } = req.body;
 
   try {
-
-    if (!billingData || billingData.length === 0) { // Check if billingData array is empty.
-      return res.status(400).json({ message: "Invalid payload: No data provided" });
+    if (!billingData || billingData.length === 0) {
+      // Check if billingData array is empty.
+      return res
+        .status(400)
+        .json({ message: "Invalid payload: No data provided" });
     }
 
     const generatedBills = [];
     // Loop through each billing object in the array and process them.
-    for (const billPayload of billingData) {  // Changed: Loop through the billingData array
+    for (const billPayload of billingData) {
+      // Changed: Loop through the billingData array
 
-       const {
-    category_id: originalCategoryId,
-    connection_size_id,
-    sewerage,
-    stp,
-    rebate,
-    currMonth,
-    prevMonth,
-    connection_type_id,
-  } = billPayload;
+      const {
+        category_id: originalCategoryId,
+        connection_size_id,
+        sewerage,
+        stp,
+        rebate,
+        currMonth,
+        prevMonth,
+        connection_type_id,
+      } = billPayload;
 
-    if (!originalCategoryId || !connection_size_id || !currMonth) {
-      return res.status(400).json({ message: "Invalid payload" });
-    }
-
-    const monthData = [];
-    if (currMonth) {
-      monthData.push({ label: "Current Month", ...currMonth });
-    }
-    if (prevMonth && originalCategoryId === 1) {
-      monthData.unshift({ label: "Previous Month", ...prevMonth });
-    }
-
-    const resultDetails = [];
-    let totalBill = 0;
-
-    for (const month of monthData) {
-      let { label, consumption, reading_date, meter_status, cw } = month;
-
-      // if (!consumption) {
-      //   return res
-      //     .status(400)
-      //     .json({ message: `Consumption missing for ${label}` });
-      // }
-
-      let meterStatusData = await db.meterStatus.findOne({
-        where: {
-          id: meter_status.id,
-        },
-      });
-
-      if (meterStatusData.meter_status !== "mf") {
-        if (meterStatusData.calc_rule === "average") {
-          consumption = 19800; // Default if 'average' rule is used
-        } else {
-          consumption = 10000; // Set to zero if no valid consumption
-        }
+      if (!originalCategoryId || !connection_size_id || !currMonth) {
+        return res.status(400).json({ message: "Invalid payload" });
       }
-      if (cw && originalCategoryId !== 1) {
-        return sendErrorResponse({
-          res,
-          err: {},
-          msg: `Invalid category_id for CW flag in ${label}. If CW is true, category_id must be 1.`,
+
+      const monthData = [];
+      if (currMonth) {
+        monthData.push({ label: "Current Month", ...currMonth });
+      }
+      if (prevMonth && originalCategoryId === 1) {
+        monthData.unshift({ label: "Previous Month", ...prevMonth });
+      }
+
+      const resultDetails = [];
+      let totalBill = 0;
+
+      for (const month of monthData) {
+        let { label, consumption, reading_date, meter_status, cw } = month;
+
+      
+
+        let meterStatusData = await db.meterStatus.findOne({
+          where: {
+            id: meter_status.id,
+          },
         });
-      }
 
-      const category_id = cw ? 2 : originalCategoryId;
-      const isBulk = !(
-        connection_size_id === 1 ||
-        connection_size_id === 2 ||
-        connection_size_id === 3
-      );
+        if (meterStatusData.meter_status !== "mf") {
+          if (meterStatusData.calc_rule === "average") {
+            consumption = 19800; // Default if 'average' rule is used
+          } else {
+            consumption = 10000; // Set to zero if no valid consumption
+          }
+        }
+        if (cw && originalCategoryId !== 1) {
+          return sendErrorResponse({
+            res,
+            err: {},
+            msg: `Invalid category_id for CW flag in ${label}. If CW is true, category_id must be 1.`,
+          });
+        }
 
-      // Calculate charges based on charge_type_id
-      const data = await calculateWaterCharges({category_id,
-        connection_size_id,
-        consumption,
-        connection_type_id,
-        isBulk,});
-      console.log(data,"<------> data <------>");
-      let { waterCharges, consumptionSlabs } = await calculateWaterCharges({
-        category_id,
-        connection_size_id,
-        consumption,
-        connection_type_id,
-        isBulk,
-      });
-    console.log(consumptionSlabs," <------> consumptionSlabs ",waterCharges);
-      // Fetch additional charges
-      const minimumChargeTariff = await db.tariffConfiguration.findOne({
-        where: {
+        const category_id = cw ? 2 : originalCategoryId;
+        const isBulk = !(
+          connection_size_id === 1 ||
+          connection_size_id === 2 ||
+          connection_size_id === 3
+        );
+
+        // Calculate charges based on charge_type_id
+        const data = await calculateWaterCharges({
           category_id,
           connection_size_id,
-          charge_type_id: 4,
-        },
-      });
-      let minimumCharge = minimumChargeTariff?.ratePerThousand || 0;
-      console.log("minimumCharge", minimumCharge)
-
-      if (meterStatusData.id == 1 && category_id == 1 && connection_size_id ==1 && consumption <= 15000) {
-        waterCharges = 0;
-        minimumCharge = 0;
-        // consumptionSlabs = []
-      }
-
-      // Final water charge, applying minimum charge logic
-      const finalWaterCharge = Math.max(minimumCharge, waterCharges);
-
-      // Fetch fixed charges
-      const fixedChargeTariff = await db.tariffConfiguration.findOne({
-        where: {
+          consumption,
+          connection_type_id,
+          isBulk,
+          billPayload
+        });
+        console.log(data, "<------> data <------>");
+        let { waterCharges, consumptionSlabs } = await calculateWaterCharges({
           category_id,
           connection_size_id,
-          charge_type_id: 2,
-        },
+          consumption,
+          connection_type_id,
+          isBulk,
+          billPayload,
+        });
+        console.log(
+          consumptionSlabs,
+          " <------> consumptionSlabs ",
+          waterCharges
+        );
+        // Fetch additional charges
+        const minimumChargeTariff = await db.tariffConfiguration.findOne({
+          where: {
+            category_id,
+            connection_size_id,
+            charge_type_id: 4,
+          },
+        });
+        let minimumCharge = minimumChargeTariff?.ratePerThousand || 0;
+        console.log("minimumCharge", minimumCharge);
+
+        if (
+          meterStatusData.id == 1 &&
+          category_id == 1 &&
+          connection_size_id == 1 &&
+          consumption <= 15000
+        ) {
+          waterCharges = 0;
+          minimumCharge = 0;
+          // consumptionSlabs = []
+        }
+
+        // Final water charge, applying minimum charge logic
+        const finalWaterCharge = Math.max(minimumCharge, waterCharges);
+
+        // Fetch fixed charges
+        const fixedChargeTariff = await db.tariffConfiguration.findOne({
+          where: {
+            category_id,
+            connection_size_id,
+            charge_type_id: 2,
+          },
+        });
+
+        const fixedCharge = fixedChargeTariff?.ratePerThousand || 0;
+
+        // Fetch meter service charges
+        const meterServiceChargeTariff = await db.tariffConfiguration.findOne({
+          where: {
+            connection_size_id,
+            charge_type_id: 3,
+          },
+        });
+
+        const meterServiceCharge =
+          meterServiceChargeTariff?.ratePerThousand || 0;
+
+        // Additional charges: Sewerage, STP, IDC, and rebate
+        const sewerageCharge = sewerage
+          ? await getSewerageCharge(finalWaterCharge)
+          : 0;
+        const stpCharge = stp ? await getStpCharge(finalWaterCharge) : 0;
+        const rebate_applied = rebate ? getRebate(finalWaterCharge) : 0;
+
+        const baseBill =
+          finalWaterCharge +
+          fixedCharge +
+          meterServiceCharge +
+          sewerageCharge +
+          stpCharge;
+
+        const idcCharge = getIDC(consumption, baseBill);
+
+        // Calculate final bill for this month
+        const bill = parseFloat(
+          (baseBill + idcCharge - rebate_applied).toFixed(2)
+        );
+
+        const monthCharges = {
+          label,
+          consumption,
+          cw, // Track CW logic
+          reading_date,
+          meter_status,
+          consumptionSlabs,
+
+          fixedCharge,
+          minimumCharge,
+          waterCharge: finalWaterCharge,
+          meterServiceCharge,
+          sewerageCharge,
+          stpCharge,
+          idcCharge,
+          rebate_applied,
+          bill,
+          billwithoutIDC: baseBill,
+        };
+
+        totalBill += bill;
+        resultDetails.push(monthCharges);
+      }
+
+      // Late payment surcharge (LPS) and total bill including LPS
+      const lps = Math.round((totalBill * 10) / 100);
+      const totalBillWithLPS = totalBill + lps;
+
+      generatedBills.push({
+        detailsByMonth: resultDetails,
+        totalBill,
+        lps,
+        totalBillWithLPS,
+        ...billPayload,
       });
-
-      const fixedCharge = fixedChargeTariff?.ratePerThousand || 0;
-
-      // Fetch meter service charges
-      const meterServiceChargeTariff = await db.tariffConfiguration.findOne({
-        where: {
-          connection_size_id,
-          charge_type_id: 3,
-        },
-      });
-
-      const meterServiceCharge = meterServiceChargeTariff?.ratePerThousand || 0;
-
-      // Additional charges: Sewerage, STP, IDC, and rebate
-      const sewerageCharge = sewerage
-        ? await getSewerageCharge(finalWaterCharge)
-        : 0;
-      const stpCharge = stp ? await getStpCharge(finalWaterCharge) : 0;
-      const rebate_applied = rebate ? getRebate(finalWaterCharge) : 0;
-
-      const baseBill =
-        finalWaterCharge +
-        fixedCharge +
-        meterServiceCharge +
-        sewerageCharge +
-        stpCharge;
-
-      const idcCharge = getIDC(consumption, baseBill);
-
-      // Calculate final bill for this month
-      const bill = parseFloat(
-        (baseBill + idcCharge - rebate_applied).toFixed(2)
-      );
-
-      const monthCharges = {
-        label,
-        consumption,
-        cw, // Track CW logic
-        reading_date,
-        meter_status,
-        consumptionSlabs,
-
-        fixedCharge,
-        minimumCharge,
-        waterCharge: finalWaterCharge,
-        meterServiceCharge,
-        sewerageCharge,
-        stpCharge,
-        idcCharge,
-        rebate_applied,
-        bill,
-        billwithoutIDC: baseBill,
-      };
-
-      totalBill += bill;
-      resultDetails.push(monthCharges);
     }
-
-    // Late payment surcharge (LPS) and total bill including LPS
-    const lps = Math.round((totalBill * 10) / 100);
-    const totalBillWithLPS = totalBill + lps;
-
-    generatedBills.push({
-      detailsByMonth: resultDetails,
-      totalBill,
-      lps,
-      totalBillWithLPS,
-      ...billPayload
-    })
-  }
     // Response payload
     return sendResponse({
       res,
